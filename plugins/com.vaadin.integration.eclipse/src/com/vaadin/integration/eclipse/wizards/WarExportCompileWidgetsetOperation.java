ffa9b68414f0c4d9f00ca3e32c3e7af72bbadf49
Henri Sara <hesara@vaadin.com>
2009-12-10 07:09:59 +0000

    #3648 build widgetset when exporting WAR
    git-svn-id: https://dev.vaadin.com/svn/integration/eclipse@10212 de684a11-831f-0410-ab27-cb86445eb682

---
+++ plugins/com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/wizards/WarExportCompileWidgetsetOperation.java
@@ -0,0 +1,38 @@
+package com.vaadin.integration.eclipse.wizards;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jst.j2ee.datamodel.properties.IJ2EEComponentExportDataModelProperties;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelOperation;
+
+import com.vaadin.integration.eclipse.builder.WidgetsetBuildManager;
+import com.vaadin.integration.eclipse.util.VaadinPluginUtil;
+
+public class WarExportCompileWidgetsetOperation extends
+        AbstractDataModelOperation {
+
+    @Override
+    public IStatus execute(IProgressMonitor monitor, IAdaptable info)
+            throws ExecutionException {
+        IStatus stat = OK_STATUS;
+
+        // ask about compiling widgetset if widgetset builds are not suspended
+        Object component = getDataModel().getProperty(
+                IJ2EEComponentExportDataModelProperties.COMPONENT);
+        if (component instanceof IVirtualComponent) {
+            IProject project = ((IVirtualComponent) component).getProject();
+            if (VaadinPluginUtil.isWidgetsetDirty(project)) {
+                WidgetsetBuildManager.runWidgetSetBuildTool(project, true,
+                        monitor);
+            }
+        }
+
+        return stat;
+
+    }
+
+}
