e03e3972ff4a7c3cc26459262e866e201c0e978b
Artur Signell <artur@vaadin.com>
2010-10-26 09:30:59 +0000

    Build files for Eclipse Integration Add-on
    git-svn-id: https://dev.vaadin.com/svn/integration/eclipse@15707 de684a11-831f-0410-ab27-cb86445eb682

---
+++ build/jarbundle-site.xml.tpl
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<site>
+   <description url="http://vaadin.com/eclipse/">
+      Update site for Vaadin.
+   </description>
+   <feature url="features/com.vaadin.integration.eclipse.jarbundle_@com.vaadin.integration.eclipse.jarbundle-version@.jar" id="com.vaadin.integration.eclipse.jarbundle" version="@com.vaadin.integration.eclipse.jarbundle-version@">
+      <category name="base"/>
+   </feature>
+   <feature url="features/com.vaadin.integration.eclipse_@com.vaadin.integration.eclipse-version@.jar" id="com.vaadin.integration.eclipse" version="@com.vaadin.integration.eclipse-version@">
+      <category name="base"/>
+   </feature>
+   <feature url="features/com.vaadin.integration.eclipse.manual_@com.vaadin.integration.eclipse.manual-version@.jar" id="com.vaadin.integration.eclipse.manual" version="@com.vaadin.integration.eclipse.manual-version@">
+      <category name="base"/>
+   </feature>
+   <feature url="features/com.vaadin.wysiwyg.eclipse_@com.vaadin.wysiwyg.eclipse-version@.jar" id="com.vaadin.wysiwyg.eclipse" version="@com.vaadin.wysiwyg.eclipse-version@">
+      <category name="base"/>
+   </feature>
+   <category-def name="base" label="Vaadin"/>
+</site>
---
+++ build/src/com/vaadin/integration/eclipse/tools/EclipsePluginDownloader.java
@@ -0,0 +1,104 @@
+package com.vaadin.integration.eclipse.tools;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+
+public class EclipsePluginDownloader {
+    private static final String STABLE_ECLIPSE_SITE_URL = "http://vaadin.com/eclipse";
+
+    public static void main(String[] args) throws IOException {
+        downloadEclipsePlugin(args[0]);
+    }
+
+    public static void downloadEclipsePlugin(String baseDir) throws IOException {
+        System.out.println("Downloading Eclipse plugins and features from "
+                + STABLE_ECLIPSE_SITE_URL + " to " + baseDir);
+        URL url = new URL(STABLE_ECLIPSE_SITE_URL + "/site.xml");
+
+        String sitexml = IOUtils.toString(url.openStream());
+        // Remove all comments
+        Matcher n = Pattern.compile("(\r|\n)", Pattern.MULTILINE).matcher(
+                sitexml);
+        sitexml = n.replaceAll("");
+        sitexml = sitexml.replaceAll("<!--(.*?)-->", "");
+        // Find the version for id="com.vaadin.integration.eclipse"
+        Pattern p = Pattern
+                .compile("<feature.*?url=\"(.*?)\" id=\"(.*?)\" version=\"(.*?)\"");
+        Matcher m = p.matcher(sitexml);
+        // System.out.println("site.xml:");
+        // System.out.println(sitexml);
+
+        int found = 0;
+
+        while (m.find()) {
+            found++;
+
+            String relativeFeatureUrl = m.group(1);
+            String relativePluginUrl = relativeFeatureUrl.replace("features/",
+                    "plugins/");
+            String pluginId = m.group(2);
+            String version = m.group(3);
+
+            if (pluginId.equals("com.vaadin.integration.eclipse.manual")) {
+                // Manual feature and plugin names do not match...
+                relativePluginUrl = relativePluginUrl.replace(
+                        "com.vaadin.integration.eclipse.manual",
+                        "com.vaadin.manual");
+            }
+
+            if (pluginId.equals("com.vaadin.integration.eclipse")) {
+                System.out.println("Latest stable Eclipse plugin version is "
+                        + version);
+            }
+
+            downloadFeature(baseDir, relativeFeatureUrl);
+            downloadPlugin(baseDir, relativePluginUrl);
+        }
+
+        if (found == 0) {
+            throw new IOException(
+                    "Unable to find out latest stable plugin version");
+        }
+
+    }
+
+    private static void downloadPlugin(String baseDir, String relativePluginUrl)
+            throws IOException {
+        downloadFile(STABLE_ECLIPSE_SITE_URL + "/" + relativePluginUrl, baseDir
+                + File.separator + relativePluginUrl);
+
+    }
+
+    /**
+     * Downloads the file if it does not already exist.
+     * 
+     * @param baseDir
+     * @param relativeFeatureUrl
+     * @throws IOException
+     */
+    private static void downloadFile(String url, String targetFilename)
+            throws IOException {
+        File targetFile = new File(targetFilename);
+        if (targetFile.exists()) {
+            return;
+        }
+
+        System.out.println("Downloading " + url);
+
+        IOUtils.copy(new URL(url).openStream(), new FileOutputStream(
+                targetFilename));
+    }
+
+    private static void downloadFeature(String baseDir,
+            String relativeFeatureUrl) throws IOException {
+
+        downloadFile(STABLE_ECLIPSE_SITE_URL + "/" + relativeFeatureUrl,
+                baseDir + File.separator + relativeFeatureUrl);
+    }
+}
