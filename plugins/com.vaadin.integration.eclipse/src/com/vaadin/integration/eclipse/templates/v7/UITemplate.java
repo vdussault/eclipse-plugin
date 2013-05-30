34658dc5e5d8961e769fd9d09f75a752e34809d4
Henri Sara <hesara@vaadin.com>
2013-05-30 12:06:48 +0000

    Generate an example of push in UI class (#11763)
    git-svn-id: https://dev.vaadin.com/svn/integration/eclipse@26014 de684a11-831f-0410-ab27-cb86445eb682

--- plugins/com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/templates/v7/UITemplate.java
+++ plugins/com.vaadin.integration.eclipse/src/com/vaadin/integration/eclipse/templates/v7/UITemplate.java
@@ -30,7 +30,11 @@ public class UITemplate {
   protected final String TEXT_7 = "\")";
   protected final String TEXT_8 = NL + "@Push";
   protected final String TEXT_9 = NL + "public class ";
-  protected final String TEXT_10 = " extends UI {" + NL + "" + NL + "\t@Override" + NL + "\tprotected void init(VaadinRequest request) {" + NL + "\t\tfinal VerticalLayout layout = new VerticalLayout();" + NL + "\t\tlayout.setMargin(true);" + NL + "\t\tsetContent(layout);" + NL + "" + NL + "\t\tButton button = new Button(\"Click Me\");" + NL + "\t\tbutton.addClickListener(new Button.ClickListener() {" + NL + "\t\t\tpublic void buttonClick(ClickEvent event) {" + NL + "\t\t\t\tlayout.addComponent(new Label(\"Thank you for clicking\"));" + NL + "\t\t\t}" + NL + "\t\t});" + NL + "\t\tlayout.addComponent(button);" + NL + "\t}" + NL + "" + NL + "}";
+  protected final String TEXT_10 = " extends UI {" + NL + "" + NL + "\tprivate VerticalLayout layout;" + NL + "" + NL + "\t@Override" + NL + "\tprotected void init(VaadinRequest request) {" + NL + "\t\tlayout = new VerticalLayout();" + NL + "\t\tlayout.setMargin(true);" + NL + "\t\tsetContent(layout);" + NL + "" + NL + "\t\tButton button = new Button(\"Click Me\");" + NL + "\t\tbutton.addClickListener(new Button.ClickListener() {" + NL + "\t\t\tpublic void buttonClick(ClickEvent event) {" + NL + "\t\t\t\tlayout.addComponent(new Label(\"Thank you for clicking\"));" + NL + "\t\t\t}" + NL + "\t\t});" + NL + "\t\tlayout.addComponent(button);";
+  protected final String TEXT_11 = NL + NL + "\t\t// Start a background thread which will periodically push hello messages" + NL + "\t\tnew Thread(new HelloService()).start();";
+  protected final String TEXT_12 = NL + "\t}" + NL;
+  protected final String TEXT_13 = NL + "\tpublic class HelloService implements Runnable {" + NL + "\t\t@Override" + NL + "\t\tpublic void run() {" + NL + "\t\t\tfor (int i = 0; i < 10; i++) {" + NL + "\t\t\t\ttry {" + NL + "\t\t\t\t\tThread.sleep(1000);" + NL + "\t\t\t\t} catch (InterruptedException e) {" + NL + "\t\t\t\t\treturn;" + NL + "\t\t\t\t}" + NL + "\t\t\t\taccess(new Runnable() {" + NL + "\t\t\t\t\t@Override" + NL + "\t\t\t\t\tpublic void run() {" + NL + "\t\t\t\t\t\tlayout.addComponent(new Label(\"Server says hello!\"));" + NL + "\t\t\t\t\t}" + NL + "\t\t\t\t});" + NL + "\t\t\t}" + NL + "\t\t\taccess(new Runnable() {" + NL + "\t\t\t\t@Override" + NL + "\t\t\t\tpublic void run() {" + NL + "\t\t\t\t\tlayout.addComponent(new Label(" + NL + "\t\t\t\t\t\t\t\"Server is tired and will no longer greet you\"));" + NL + "\t\t\t\t}" + NL + "\t\t\t});" + NL + "\t\t}" + NL + "\t}" + NL;
+  protected final String TEXT_14 = NL + "}";
 
     public String generate(String applicationPackage, String applicationName,
         String uiOrApplicationClass, String uiTheme, boolean push)
@@ -57,6 +61,14 @@ public class UITemplate {
     stringBuffer.append(TEXT_9);
     stringBuffer.append( uiOrApplicationClass );
     stringBuffer.append(TEXT_10);
+     if (push) {
+    stringBuffer.append(TEXT_11);
+     } 
+    stringBuffer.append(TEXT_12);
+     if (push) {
+    stringBuffer.append(TEXT_13);
+     } 
+    stringBuffer.append(TEXT_14);
     return stringBuffer.toString();
   }
 }
